0b92eb35389b22d8c726d3b62ccd4e9781b7f45b
AimsLynne <130839244+AimsLynne@users.noreply.github.com>
2023-12-20 16:05:29 +0000

    Added JavaScript and Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generateStory(event) {
+  event.preventDefault();
 
+  new Typewriter("#story", {
+    strings: "Contents will go here",
+    autoStart: true,
+    cursor: "",
+  });
+}
+
+let storyFormElement = document.querySelector("#story-generator-form");
+storyFormElement.addEventListener("submit", generateStory);
